b81bbb30e739ae83ddaa74c8f90050ed161c91d3
Aleksei Zhuikov <design.azv@gmail.com>
2023-01-23 17:49:29 +0300

    added cats in section main

--- js/main.js
+++ js/main.js
@@ -14,3 +14,12 @@ addBtnEl.addEventListener("click", function (event) {
 closePopupFormEl.addEventListener("click", function (event) {
 	popupEL.classList.remove("popup_active");
 });
+
+/** Задаем ширину карточек */
+
+const cards = document.querySelectorAll(".card");
+
+for (let i = 0, cnt = cards.length; i < cnt; i++) {
+	const width = cards[i].clientWidth;
+	cards[i].style.height = width * 0.6 + "px";
+}
