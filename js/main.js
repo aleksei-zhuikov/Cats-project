def236e3ee84f5472becc5b1a3d59cd7e89ab6c3
Aleksei Zhuikov <design.azv@gmail.com>
2023-01-23 16:38:57 +0300

    added js  open/close popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const addBtnEl = document.getElementById("add"),
+	popupEL = document.querySelector(".popup"),
+	closePopupFormEl = document.querySelector(".popup__close"),
+	formBtnAddCat = document.querySelector(".form__btn");
+
+addBtnEl.addEventListener("click", function (event) {
+	event.preventDefault();
+
+	if (!popupEL.classList.contains("popup_active")) {
+		popupEL.classList.add("popup_active");
+	}
+});
+
+closePopupFormEl.addEventListener("click", function (event) {
+	popupEL.classList.remove("popup_active");
+});
